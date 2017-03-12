acac49e29f82d0b7978a837cb800f67dd89f56cc
Robert Ball <robert.ball.1989@gmail.com>
2017-03-12 15:40:09 +0000

    Completed deck 'seeds' document and created PlayerTest.

---
+++ specs/PlayerTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import cardgame.*;
+
+public class PlayerTest{
+  Player player;
+  Card card;
+
+  @Before
+  public void before(){
+    player = new Player("Remy");
+    card1 = new Card(Suit.HEARTS, Face.EIGHT, 8);
+    card2 = new Card(Suit.DIAMONDS, Face.FIVE, 5);
+  }
